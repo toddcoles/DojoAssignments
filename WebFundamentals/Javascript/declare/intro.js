bd1f1522134f79de9a09547a1dca6fb3c7b299dc
ToddColes <todd_coles@hotmail.com>
2017-10-12 17:20:32 -0700

    Added the declare file

---
+++ WebFundamentals/Javascript/declare/intro.js
@@ -0,0 +1,25 @@
+
+    var FirstNumber = 120.14;
+    var SecondNumber = -240;
+    var ThirdNumber = 30.99;
+    var FourthNumber = 1000;
+   
+    var FirstName = 'Todd';
+    var SecondName = 'Jason';
+    var LastName = 'Coles';
+    
+    var myAnswer = true;
+    var yourAnswer = false;
+    
+    var email;
+
+    console.log('First number is: ', FirstNumber);
+    console.log('Second number is: ', SecondNumber);
+    console.log('Third number is: ', ThirdNumber);
+    console.log('Fourth number is: ', FourthNumber);
+    console.log('The sum of the number is: ', FirstNumber+SecondNumber+ThirdNumber+FourthNumber);
+
+    console.log('My name is: ', FirstName + ' ' + SecondName +' ' + LastName);
+
+    console.log('True = ', myAnswer);
+    console.log('False = ', yourAnswer);
