ccc022ec393396c010098c26ef165029faf4f6ad
ToddColes <todd_coles@hotmail.com>
2017-10-15 18:52:22 -0700

    Added Fancy project

---
+++ WebFundamentals/Javascript/Fancy/fancy.js
@@ -0,0 +1,23 @@
+var myArray = ['James','Jill','Jane','Jack'];
+
+for(var index in myArray) {
+    console.log( index + " : " + myArray[index]);
+  } console.log('');
+  
+  for(var index in myArray) {
+    console.log( index + " -> " + myArray[index]);
+  } console.log('');
+  
+  for(var index in myArray) {
+    console.log( index + " => " + myArray[index]);
+  } console.log('');
+  
+  for(var index in myArray) {
+    console.log( index + " :: " + myArray[index]);
+  } console.log('');
+  
+  for(var index in myArray) {
+    console.log( index + " ----- " + myArray[index]);
+  }
+  
+
