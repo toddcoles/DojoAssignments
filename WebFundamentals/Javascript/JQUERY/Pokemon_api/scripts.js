f29309efa7a7b306279f0cab28b73d9ce63cdad2
ToddColes <todd_coles@hotmail.com>
2017-10-26 14:16:53 -0700

    Added the Pokemon project

--- WebFundamentals/Javascript/JQUERY/Pokemon_api/scripts.js
+++ WebFundamentals/Javascript/JQUERY/Pokemon_api/scripts.js
@@ -1,13 +1,16 @@
 $(document).ready(function() {
-    // var pokedirect = "https://pokeapi.co/api/v2/pokemon/16";
-    // $.get(pokedirect, function(data){
-    //     console.log(data);
-    //     $(".wrapper").append(data);
-    //     }, "json");
-
 
         for (var x = 1; x < 152; x++) {
-            $('.wrapper').append(`<img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/shiny/${x}.png">`) 
+            $('.poke').append(`<img id=${x} src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/shiny/${x}.png">`) 
           }
+        
+          $('img').click(function(){
+            var id = $(this).attr('id');
+            $.get("https://pokeapi.co/api/v2/pokemon/" + id +"/", function(response){
+            console.log(response);
+            var pokeID = response.id;
+            $('.side').append("<div class='inside_pic'><img src='https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/shiny/"+ pokeID + ".png'><br><h2>" +response.name + "</h2><p> Types " + response.type + "</p><p> <h3>Height: </h3>" + response.height + "</p><p><h3>Weight: </h3>" + response.weight + "</p></div>");
+            }, "json");
+        })
 
 });
