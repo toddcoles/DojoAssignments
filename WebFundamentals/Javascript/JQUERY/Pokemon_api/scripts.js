782d60b77b678cf93352b509a3d0d32e9ce81490
ToddColes <todd_coles@hotmail.com>
2017-10-27 11:06:00 -0700

    Updated Pokemon_api project

--- WebFundamentals/Javascript/JQUERY/Pokemon_api/scripts.js
+++ WebFundamentals/Javascript/JQUERY/Pokemon_api/scripts.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
 
         for (var x = 1; x < 152; x++) {
-            $('.poke').append(`<img id=${x} src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/shiny/${x}.png">`) 
+            $('.poke').append(`<img id=${x} src="https://pokeapi.co/media/sprites/pokemon/${x}.png">`) 
           }
         
           $('img').click(function(){
@@ -9,7 +9,20 @@ $(document).ready(function() {
             $.get("https://pokeapi.co/api/v2/pokemon/" + id +"/", function(response){
             console.log(response);
             var pokeID = response.id;
-            $('.side').append("<div class='inside_pic'><img src='https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/shiny/"+ pokeID + ".png'><br><h2>" +response.name + "</h2><p> Types " + response.type + "</p><p> <h3>Height: </h3>" + response.height + "</p><p><h3>Weight: </h3>" + response.weight + "</p></div>");
+            var myString +=`
+            <div class='inside_pic'>
+            <img src='https://pokeapi.co/media/sprites/pokemon/${pokeID}.png'>
+            <h2>${response.name}</h2>
+            Types <ul>`
+            for(var x = 0; x <= response.types.length){
+              myString+=`<li>${response.types[x].name}</li>`;
+            }
+              
+            myString+=`<h3>Height: 
+            </h3>${response.height}
+            <h3>Weight: </h3>${response.weight}
+            </div>`);
+            $('.side').html(myString);
             }, "json");
         })
 
