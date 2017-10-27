f090378b2a9662c1c32fe648c6e45d07cde6cfd5
ToddColes <todd_coles@hotmail.com>
2017-10-27 14:25:13 -0700

    Added weather API project

---
+++ WebFundamentals/Javascript/JQUERY/Weather/scripts.js
@@ -0,0 +1,17 @@
+$(document).ready(function() {
+
+        var APIkey = '2c3f125fc613625b6f805f6a262add06';
+        var location = '';
+        var weatherURL = 'http://api.openweathermap.org/data/2.5/weather?q=';// + location + '&&appid=' + APIkey;
+
+        $('form').submit(function(){
+            var location = $('#location').val();
+       
+        $.get(weatherURL + location + '&&appid=' + APIkey, function(weatherResult){
+            console.log(weatherResult);
+            var newTemp = (1.8 * (weatherResult.main.temp - 273)) + 32;
+        $('#temp').html(`<h1>${weatherResult.name}</h1><h2>Temperature: ${newTemp.toFixed(2)} F</h2>`);
+        }, 'json');
+
+    });
+});
