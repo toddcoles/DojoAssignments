4181c9fa6a5d9d27241af7bb212a93b284f10005
ToddColes <todd_coles@hotmail.com>
2017-11-04 11:21:59 -0700

    Adding the Find Characters project

---
+++ Python/WEEK1/Find_Characters.py
@@ -0,0 +1,15 @@
+# input
+word_list = ['hello','world','my','name','is','Anna']
+char = 'o'
+new_list = []
+for item in word_list:
+	if char in item:
+		# output
+		new_list.append(item)
+
+for words in new_list:
+	print words,
+
+
+
+
