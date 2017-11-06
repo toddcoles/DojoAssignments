559fd9b57b77e81c423c0164c983469af4f4574e
ToddColes <todd_coles@hotmail.com>
2017-11-06 15:16:50 -0700

    Adding Names project

---
+++ Python/WEEK1/Names.py
@@ -0,0 +1,44 @@
+#Make a dictionary about myself that contains name, age, country of birth, favorite language
+#Make a function that that extracts the data
+
+students = [
+     {'first_name':  'Michael', 'last_name' : 'Jordan'},
+     {'first_name' : 'John', 'last_name' : 'Rosales'},
+     {'first_name' : 'Mark', 'last_name' : 'Guillen'},
+     {'first_name' : 'KB', 'last_name' : 'Tonel'}
+]
+
+users = {
+ 'Students': [
+     {'first_name':  'Michael', 'last_name' : 'Jordan'},
+     {'first_name' : 'John', 'last_name' : 'Rosales'},
+     {'first_name' : 'Mark', 'last_name' : 'Guillen'},
+     {'first_name' : 'KB', 'last_name' : 'Tonel'}
+  ],
+ 'Instructors': [
+     {'first_name' : 'Michael', 'last_name' : 'Choi'},
+     {'first_name' : 'Martin', 'last_name' : 'Puryear'}
+  ]
+ }
+
+
+def names(x):
+	for dics in x:
+		print dics['first_name'] + " " +dics['last_name']
+
+def show_all(users):
+    for role in users:
+        counter = 0
+        print role
+        for person in users[role]:
+            counter += 1
+            first_name = person['first_name'].upper()
+            last_name = person['last_name'].upper()
+            length = len(first_name) + len(last_name)
+            print "{} - {} {} - {}".format(counter, first_name, last_name, length)
+
+
+names(students)
+show_all(users)
+
+
